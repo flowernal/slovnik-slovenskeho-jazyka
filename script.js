9da9598ad06b42f2275afe163db67bc53ac2a9d2
BramboraSK <majojo992@gmail.com>
2022-03-29 17:55:42 +0200

    Dokončenie stránky, pridanie logiky pre export funkciu

--- script.js
+++ script.js
@@ -10,7 +10,53 @@ const toggleAll = source => {
 }
 
 const exportFile = () => {
+    let checkboxes = document.getElementsByName("flag");
+    let flags = [];
+    let includeWordsWithoutFlags = false;
+
+    for (let checkbox of checkboxes)
+        if (checkbox.checked)
+            if (checkbox.value === "0")
+                includeWordsWithoutFlags = true;
+            else
+                flags.push(checkbox.value);
+
     fetch("https://slovnik.brambora.sk/slovnik.txt")
         .then(data => data.text())
-        .then(text => console.log(text));
+        .then(text => {
+            let minlength = document.getElementById("minlength");
+            let maxlength = document.getElementById("maxlength");
+
+            if (minlength.value.length && !maxlength.value.length) maxlength.value = minlength.value;
+
+            let words = text.split('\n').filter(word => {
+                let split = word.split('/');
+
+                if (includeWordsWithoutFlags && !split[1])
+                    return true;
+                else if (split[1])
+                    for (let flag of flags)
+                        if (split[1].includes(flag))
+                            return true;
+
+                return false;
+            });
+
+            if (minlength.value.length) words = words.filter(word => word.length >= minlength.value && word.length <= maxlength.value);
+
+            let element = document.createElement("a");
+
+            element.setAttribute("href",
+                "data:text/plain;charset=utf-8," + encodeURIComponent(words.map(word => word.split('/')[0]).join("\n"))
+            );
+
+            element.setAttribute("download", "exported.txt");
+
+            element.style.display = "none";
+            document.body.appendChild(element);
+
+            element.click();
+
+            document.body.removeChild(element);
+        });
 }
