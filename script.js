09c8c73d8c7e3f5db71911b735f5c315fe65daed
flowernal <majojo992@gmail.com>
2022-12-01 18:23:57 +0100

    🎨 Refactor + oprava funkcnosti nastaveni

--- script.js
+++ script.js
@@ -3,66 +3,81 @@
  * @param {HTMLInputElement} source
  */
 const toggleAll = source => {
-    let checkboxes = document.getElementsByName("flag");
+	let checkboxes = document.getElementsByName("flag");
 
-    for (let checkbox of checkboxes)
-        checkbox.checked = source.checked;
+	for (let checkbox of checkboxes)
+		checkbox.checked = source.checked;
 }
 
 const exportFile = () => {
-    let checkboxes = document.getElementsByName("flag");
-    let flags = [];
-    let includeWordsWithoutFlags = false;
+	let checkboxes = document.getElementsByName("flag");
+	let flags = [];
+	let includeWordsWithoutFlags = false;
 
-    for (let checkbox of checkboxes) {
-        if (checkbox.checked) {
-            if (checkbox.value === "0") {
-                includeWordsWithoutFlags = true;
-            }
-            else {
-                flags.push(checkbox.value);
-            }
-        }
-    }
+	for (let checkbox of checkboxes) {
+		if (checkbox.checked) {
+			if (checkbox.value === "0") {
+				includeWordsWithoutFlags = true;
+			}
+			else {
+				flags.push(checkbox.value);
+			}
+		}
+	}
 
-    fetch("slovnik.txt")
-        .then(data => data.text())
-        .then(text => {
-            let minlength = document.getElementById("minlength");
-            let maxlength = document.getElementById("maxlength");
-            let removeuppercase = document.getElementById("removeuppercase").checked;
+	fetch("slovnik.txt")
+		.then(data => data.text())
+		.then(text => {
+			const minLength = document.getElementById("minLength");
+			const maxLength = document.getElementById("maxLength");
+			const removeUppercase = document.getElementById("removeUppercase").checked;
 
-            if (minlength.value.length && !maxlength.value.length) maxlength.value = minlength.value;
+			let words = text.split("\n").filter(word => {
+				const split = word.split("/");
 
-            let words = text.split('\n').filter(word => {
-                let split = word.split('/');
+				if (includeWordsWithoutFlags && !split[1]) {
+					return true;
+				}
+				else if (split[1]) {
+					for (let flag of flags) {
+						if (split[1].includes(flag)) {
+							return true;
+						}
+					}
+				}
 
-                if (includeWordsWithoutFlags && !split[1])
-                    return true;
-                else if (split[1])
-                    for (let flag of flags)
-                        if (split[1].includes(flag))
-                            return true;
+				return false;
+			}).map(word => word.split("/")[0]);
 
-                return false;
-            }).map(word => word.split('/')[0]);
+			words.filter(word => {
+				if (minLength.value.length && word.length < Number(minLength.value)) {
+					return false;
+				}
 
-            if (minlength.value.length) words = words.filter(word => word.length >= minlength.value && word.length <= maxlength.value);
-            if (removeuppercase) words = words.filter(word => word.toLowerCase() === word)
+				if (maxLength.value.length && word.length > Number(maxLength.value)) {
+					return false;
+				}
+				
+				if (removeUppercase && word.toLowerCase() !== word) {
+					return false;
+				}
 
-            let element = document.createElement("a");
+				return true;
+			});
 
-            element.setAttribute("href",
-                "data:text/plain;charset=utf-8," + encodeURIComponent(words.join("\n"))
-            );
+			let element = document.createElement("a");
 
-            element.setAttribute("download", "exported.txt");
+			element.setAttribute("href",
+				"data:text/plain;charset=utf-8," + encodeURIComponent(words.join("\n"))
+			);
 
-            element.style.display = "none";
-            document.body.appendChild(element);
+			element.setAttribute("download", "exported.txt");
 
-            element.click();
+			element.style.display = "none";
+			document.body.appendChild(element);
 
-            document.body.removeChild(element);
-        });
+			element.click();
+
+			document.body.removeChild(element);
+		});
 }
